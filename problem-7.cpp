d1ce9546197047f0f0ea2d08671bc3f21df1e0ca
Dhruv <dhruvj1@protonmail.com>
2022-05-29 16:05:19 +0530

    Create problem-7.cpp

---
+++ problem-7.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+
+void printArr(vector<bool> arr) {
+  for (int i = 0; i < arr.size(); i++) {
+        std::cout << arr.at(i) << ' ';
+    }
+}
+void simpleSieve(ll n, vector<int> &nprimes) {
+  vector<bool> isPrime(n+1, true);
+  isPrime[0] = isPrime[1] = false;
+  for (ll i=2; i*i < n; i++) { 
+    if (isPrime[i]) {
+      for (ll j=i*i; j<=n; j+=i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+ 
+  for (ll i=2; i<=n; i++) {
+    if (isPrime[i]) {
+      nprimes.push_back(i);
+      }
+  } 
+}
+
+void segmentedSieve(ll n, vector<int> &nprimes) {
+  vector<int> primes;
+  ll s  = sqrt(n);
+  simpleSieve(s, primes);
+  ll high = 0;
+  for (ll low=2; low <= n; low += s) {
+    high = min(low+s-1, n-1);
+    vector<bool> isPrime(high-low+1, true);
+    for (ll p: primes) {
+      ll m = (low/p)*p; 
+      
+      if (m < low) m+=p;
+      if (m == p) m+=p;
+
+      for (ll i=m; i<=high; i += p) {
+        isPrime[i-low] = false;
+      }
+    }
+    for (int i = 0; i < isPrime.size(); i++) {
+      if (isPrime[i])
+      {
+          nprimes.push_back(i + low);
+      }
+    }
+  }
+}
+
+int main() {
+  vector<int> nprimes;
+  ll t = 10001;
+  ll n = t*log(t*log(t));
+  segmentedSieve(n, nprimes);
+  cout << nprimes.size();
+  cout << endl;
+  cout << nprimes[t-1];
+    }
